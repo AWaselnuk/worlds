32d8aca2ea7e17611fbf4359fa6dd16ee4061c81
Adam Waselnuk <adam.waselnuk@gmail.com>
2018-02-24 10:53:24 -0500

    First render and build scripts: TS, React, Yarn, parcel-bundler

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+const App = (props) => {
+  return (
+    <h1>Hello World</h1>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
