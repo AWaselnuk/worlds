9f49a9d331832b0ac5ea5ea7c22aac8ab9b44081
Adam Waselnuk <adam.waselnuk@gmail.com>
2018-02-25 18:26:08 -0500

    Extract GridData class for non-view stuff

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,6 +3,7 @@ import { create as randomSeed } from 'random-seed';
 import { flatten } from 'lodash';
 import Cell from './Cell';
 import { valueToType as valueToCellColor } from './CellType';
+import { GridData, Grid as GridT } from './GridData';
 
 interface Props {
   seed: string;
@@ -11,22 +12,17 @@ interface Props {
 
 interface State {}
 
-type CellData = number;
-type RowData = Array<CellData>;
-type GridData = Array<RowData>;
-
 export default class Grid extends React.PureComponent<Props, State> {
   private seed: string;
   private random: any; // TODO: figure out how to type this
-  private data: GridData;
+  private data: GridT;
 
   constructor (props: Props) {
     super(props);
 
     const { seed, size } = props;
     this.seed = seed;
-    this.random = randomSeed(seed);
-    this.data = this.generateGridData(size);
+    this.data = new GridData(seed, size).data;
     console.info(this.data);
   }
 
@@ -43,18 +39,8 @@ export default class Grid extends React.PureComponent<Props, State> {
     );
   }
 
-  private static Cell (value: CellData, index: number): JSX.Element {
+  private static Cell (value: number, index: number): JSX.Element {
     const color = valueToCellColor(value);
     return <Cell key={index} color={color} size={40} value={value} />;
-  }
-
-  private generateGridData (size: number): GridData {
-    const grid = new Array(size).fill(size);
-    return grid.map(this.generateRowData.bind(this));
-  }
-
-  private generateRowData (size: number): RowData {
-    const row = new Array(size).fill(0);
-    return row.map((_) => this.random.range(10));
   }
 }
---
+++ src/GridData.ts
@@ -0,0 +1,29 @@
+import { create as randomSeed } from 'random-seed';
+
+export type Grid = Array<Row>;
+type Row = Array<Cell>;
+type Cell = number;
+
+export class GridData {
+  seed: string;
+  size: number;
+  random: any; // TODO: figure out how to type this
+  data: Grid;
+
+  constructor (seed: string, size: number) {
+    this.seed = seed;
+    this.size = size;
+    this.random = randomSeed(seed);
+    this.data = this.generate();
+  }
+
+  private generate (): Grid {
+    const grid = new Array(this.size).fill(this.size);
+    return grid.map(this.generateRow.bind(this));
+  }
+
+  private generateRow (): Row {
+    const row = new Array(this.size).fill(0);
+    return row.map((_) => this.random.range(10));
+  }
+}
